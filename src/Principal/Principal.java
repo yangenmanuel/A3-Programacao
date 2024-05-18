80d9fcb9de409399030a1984fd278654418ab49c
Yang Enmanuel <enmanuelcalzat@gmail.com>
2024-05-18 17:22:37 -0300

    changed ide to netbeans

---
+++ src/Principal/Principal.java
@@ -0,0 +1,14 @@
+package Principal;
+
+import View.MainScreen;
+import View.TelaPrincipal;
+
+public class Principal {
+
+    public static void main(String[] args) {
+//        TelaPrincipal objetotela = new TelaPrincipal();
+        MainScreen objetotela = new MainScreen();
+        objetotela.setVisible(true);
+    }
+    
+}
